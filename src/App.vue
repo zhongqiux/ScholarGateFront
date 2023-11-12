39c801fb07a04ccf5d03cf7e97468962b143d5f1
zhongqiu <1848115959@qq.com>
2023-11-12 10:40:00 +0800

    init

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,18 @@
 <script setup lang="ts">
 import { RouterView } from 'vue-router';
+import Header from "@/components/Header.vue"
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <div class="common-layout">
+    <el-container>
+      <el-header><Header></Header></el-header>
+      <el-main>
+        <router-view></router-view>
+      </el-main>
+    </el-container>
   </div>
-  <router-view></router-view>
+  
 </template>
 
 <style scoped>
---
+++ src/components/Header.vue
@@ -0,0 +1,14 @@
+<template>
+<div>Header</div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+export default defineComponent({
+	name:"Header",
+})
+</script>
+
+<style scoped>
+
+</style>
