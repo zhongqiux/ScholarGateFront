95dc61377af25f711af3e5f91d8b3ec1579ce650
曹月耀 <21371444>
2023-11-16 13:13:15 +0800

    检索详情 ver1.0

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -2,4 +2,5 @@ export const routes = [
     { path: '/', name: '', title: '首页', component: ()=>import('@/components/HelloWorld.vue') },
 	{ path: '/index', name: 'index', title: '首页', component: ()=>import('@/components/HelloWorld.vue') },
 	{ path: '/main', name: '', title: 'main', component: ()=>import('@/pages/MainPage.vue') },
+	{path: '/explore', name: 'explore', title: '检索结果', component: ()=>import('@/pages/SearchDetail.vue')},
 ]
