6f72b7c18db53a5f9449ea9486a331aae181338e
Yuming <anyumingyan@gmail.com>
2023-11-19 14:14:38 +0800

    update1

---
+++ src/components/Avatar.vue
@@ -0,0 +1,9 @@
+<template>
+    <el-avatar src="https://cube.elemecdn.com/0/88/03b0d39583f48206768a7534e55bcpng.png" :size="150"/>
+</template>
+<script lang="ts" setup>
+
+</script>
+  
+<style scoped></style>
+  
--- src/router/routes.ts
+++ src/router/routes.ts
@@ -2,4 +2,6 @@ export const routes = [
     { path: '/', name: '', title: '首页', component: ()=>import('@/components/HelloWorld.vue') },
 	{ path: '/index', name: 'index', title: '首页', component: ()=>import('@/components/HelloWorld.vue') },
 	{ path: '/main', name: '', title: 'main', component: ()=>import('@/pages/MainPage.vue') },
+	{ path: '/person', name: '', title: 'person', component: ()=>import('@/pages/PersonPage.vue') },
+	{ path: '/editinfo', name: '', title: 'editinfo', component: ()=>import('@/pages/EditInfoPage.vue') },
 ]
