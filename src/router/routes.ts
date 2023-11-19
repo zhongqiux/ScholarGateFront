d8830a961d7bf39fc2101739c2bf619e60479439
陈敬涛 <21371278>
2023-11-19 14:16:08 +0800

    管理员审核界面

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -2,4 +2,5 @@ export const routes = [
     { path: '/', name: '', title: '首页', component: ()=>import('@/components/HelloWorld.vue') },
 	{ path: '/index', name: 'index', title: '首页', component: ()=>import('@/components/HelloWorld.vue') },
 	{ path: '/main', name: '', title: 'main', component: ()=>import('@/pages/MainPage.vue') },
+	{ path: '/admin', name: 'admin', title: '管理审核', component: ()=>import('@/pages/AdminPage.vue')}
 ]
