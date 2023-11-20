1ac364c6e967d9e1cfaa171aa6bab2596ed53207
Zora <qy031110@gmail.com>
2023-11-20 19:28:38 +0800

    专利展示页面基本完成

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -1,12 +1,13 @@
 export const routes = [
-    { path: '/', name: '', title: '首页', component: ()=>import('@/components/HelloWorld.vue') },
-	{ path: '/index', name: 'index', title: '首页', component: ()=>import('@/components/HelloWorld.vue') },
-	{ path: '/main', name: '', title: 'main', component: ()=>import('@/pages/MainPage.vue') },
-	{ path: '/admin', name: 'admin', title: '管理审核', component: ()=>import('@/pages/AdminPage.vue')},
-    { path: '/', name: '', title: '首页', component: ()=>import('@/pages/MainPage.vue') },
-	{ path: '/index', name: 'index', title: '首页', component: ()=>import('@/pages/MainPage.vue') },
-	{ path: '/main', name: '', title: 'main', component: ()=>import('@/components/HelloWorld.vue') },
-	{ path: '/person', name: '', title: 'person', component: ()=>import('@/pages/PersonPage.vue') },
-	{ path: '/editinfo', name: '', title: 'editinfo', component: ()=>import('@/pages/EditInfoPage.vue') },
-	{ path: '/result', name: '', title: 'result', component: ()=>import('@/pages/ResultPage.vue') },
+    {path: '/', name: '', title: '首页', component: () => import('@/components/HelloWorld.vue')},
+    {path: '/index', name: 'index', title: '首页', component: () => import('@/components/HelloWorld.vue')},
+    {path: '/main', name: '', title: 'main', component: () => import('@/pages/MainPage.vue')},
+    {path: '/admin', name: 'admin', title: '管理审核', component: () => import('@/pages/AdminPage.vue')},
+    {path: '/', name: '', title: '首页', component: () => import('@/pages/MainPage.vue')},
+    {path: '/index', name: 'index', title: '首页', component: () => import('@/pages/MainPage.vue')},
+    {path: '/main', name: '', title: 'main', component: () => import('@/components/HelloWorld.vue')},
+    {path: '/person', name: '', title: 'person', component: () => import('@/pages/PersonPage.vue')},
+    {path: '/editinfo', name: '', title: 'editinfo', component: () => import('@/pages/EditInfoPage.vue')},
+    {path: '/result', name: '', title: 'result', component: () => import('@/pages/ResultPage.vue')},
+    {path: '/patent', name: '', title: 'patent', component: () => import('@/pages/PatentPage.vue')}
 ]
