8956ed369ff24fc8a412c32bcb52629b80ab734f
zhongqiu <1848115959@qq.com>
2023-12-06 20:39:41 +0800

    登录注册接口示例

--- src/API/api.ts
+++ src/API/api.ts
@@ -1,6 +1,6 @@
 import * as Type from './type';
 import axios from 'axios';
-
+import { useUserStore } from '@/store';
 axios.defaults.baseURL = import.meta.env.VITE_HOST
 axios.defaults.timeout = 3000
 
@@ -23,4 +23,47 @@ export function test(msg:String):Promise< Type.CommonReturnType > {
 
 export function hello():void{
     window.alert('hello')
+}
+
+export function login(name:string,password:string):Promise< Type.LoginReturn >{
+    return new Promise((resolve,reject)=>{
+        // axiso 自带 get 和 post 方法
+        axios.get("/login",{
+            params:{
+                name:name,
+                password:password,
+            },
+            headers:{
+                Authorization:useUserStore().token,
+            }//get请求携带登录凭证
+        }).then(res=>{
+            console.log(res);
+			resolve(res.data);
+        }).catch(err=>{
+            console.log(err)
+			reject(err)
+        })
+    })
+}
+export function register(name:string,password:string,email:string):Promise< Type.RegisterReturn >{
+    return new Promise((resolve,reject)=>{
+        // axiso 自带 get 和 post 方法
+        axios.post("/register",{
+            name:name,
+            password:password,
+            email:email,
+        }
+        ,{
+            headers:{
+                Authorization:useUserStore().token,
+            }
+        }//post请求携带登录凭证
+        ).then(res=>{
+            console.log(res);
+			resolve(res.data);
+        }).catch(err=>{
+            console.log(err)
+			reject(err)
+        })
+    })
 }
