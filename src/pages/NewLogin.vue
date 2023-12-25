7ad727dc1b2c74b1f381fe35261db622eca71031
Shuo Wenbai <1411074513@qq.com>
2023-12-25 22:04:24 +0800

    update

--- src/pages/NewLogin.vue
+++ src/pages/NewLogin.vue
@@ -1,7 +1,9 @@
 <template>
     <div>
         <div class="background"></div>
-        <div style="opacity: 0.85; color: black;">
+        <img data-v-c970699f="" src="/ScholarGateFront/src/assets/icon.png" class="w-60 h-17"
+            style="position: absolute; left: 35.5vw; top: -1vh; width: 30vw;">
+        <div style="opacity: 0.65; color: black;">
             <!-- 登陆页 -->
             <div :class="{ 'flip-front': playFlip }" class="flip-item flip-item-front">
                 <div class="register-form" style="margin-top: 20vh;">
@@ -19,7 +21,6 @@
                             <button type="submit">登录</button>
                         </div>
                         <div class="tit" style="margin-top: 3vh;">
-                            <!-- <el-button @click="handleForgotPassword">忘记密码？</el-button> -->
                             <el-button @click="play">没有账号？立即注册</el-button>
                         </div>
                     </form>
@@ -31,7 +32,7 @@
                     <h2 class="tit" style="font-weight: bold; color: black; font-size: 26px;">注册</h2>
                     <form @submit.prevent="APIregister">
                         <div class="form-group">
-                            <label for="nickname">昵称</label>
+                            <label for="nickname">账号</label>
                             <input v-model="nickname" type="text" id="nickname" required />
                         </div>
                         <div class="form-group">
@@ -60,40 +61,6 @@
                     </form>
                 </div>
             </div>
-            <!-- 忘记密码页 -->
-            <!-- <div :class="{ 'flip-back': playForget }" class="flip-item flip-item-back">
-                <div class="register-form" style="margin-top: 15vh;">
-                    <h2 class="tit" style="font-weight: bold; color: black; font-size: 26px;">忘记密码</h2>
-                    <form @submit.prevent="resetPassword">
-                        <div class="form-group">
-                            <label for="f_username">用户名</label>
-                            <input v-model="f_username" type="text" id="f_username" required />
-                        </div>
-                        <div class="form-group">
-                            <label for="f_email">邮箱</label>
-                            <input v-model="f_email" type="email" id="f_email" required />
-                        </div>
-                        <div class="form-group">
-                            <label for="verificationCode">验证码</label>
-                            <input v-model="verificationCode" type="text" id="verificationCode" style="width: 18.2vw;" required />
-                            <button type="button" @click="sendVerificationCode"
-                                :disabled="verificationCodeSent" style="font-size: 16px; width: 6vw;">发送</button>
-                        </div>
-                        <div class="form-group">
-                            <label for="newPassword">新密码</label>
-                            <input v-model="newPassword" type="password" id="newPassword" required />
-                        </div>
-                        <div class="form-group">
-                            <label for="confirmNewPassword">确认密码</label>
-                            <input v-model="confirmNewPassword" type="password" id="confirmNewPassword" required />
-                        </div>
-                        <div class="tit">
-                            <button type="submit">重置密码</button>
-                        </div>
-                        <el-button @click="handleForgotPassword">返回登录</el-button>
-                    </form>
-                </div>
-            </div> -->
         </div>
     </div>
 </template>
@@ -145,9 +112,6 @@ export default {
                     type: 'error',
                 })
             } else {
-                // this.$router.push('/')
-                // this.store.outdate = new Date(Date.now() + 60 * 60 * 1000)
-                // console.log(Date.now() + 60 * 60 * 1000)
                 login(this.username, this.pwd).then((res: Type.LoginReturn) => {
                     if (res.flag == true) {
                         this.store.outdate = Date.now() + 60 * 60 * 1000
@@ -214,165 +178,46 @@ export default {
             }
         },
 
-        // 注册旧
-        // async register() {
-        //     if (this.password !== this.confirmPassword) {
-        //         alert('两次密码不一致！');
-        //         return;
-        //     }
-        //     // 在这里执行注册逻辑，可以发送请求给服务器或者执行其他操作
-        //     const data = {
-        //         'username': this.nickname,
-        //         'password': this.password,
-        //         'email': this.email,
-        //         'code': this.verificationCode,
-        //     }
-        //     console.log(data)
-        //     // try {
-        //     //     const response = await fetch('http://dev-cn.your-api-server.com/register', {
-        //     //         method: 'POST',
-        //     //         headers: {
-        //     //             'Content-Type': 'application/json',
-        //     //         },
-        //     //         body: JSON.stringify(data),
-        //     //     })
-
-        //     //     if (response.status === 200) {
-        //     //         response.json().then(res => {
-        //     //             if (res.code === 200) {
-        //     //                 ElMessage({
-        //     //                     message: `注册成功！`,
-        //     //                     type: 'success',
-        //     //                     offset: 100,
-        //     //                 })
-        //     //                 this.nickname = ''
-        //     //                 this.email = ''
-        //     //                 this.password = ''
-        //     //                 this.confirmPassword = ''
-        //     //                 this.play()
-        //     //                 sessionStorage.setItem('showIntro', 'true')
-        //     //             } else if (res.code === 411) {
-        //     //                 ElMessage({
-        //     //                     message: `用户已存在！`,
-        //     //                     type: 'warning',
-        //     //                     offset: 100,
-        //     //                 })
-        //     //             } else if (res.code === 410) {
-        //     //                 ElMessage({
-        //     //                     message: `邮箱不合法！`,
-        //     //                     type: 'warning',
-        //     //                     offset: 100,
-        //     //                 })
-        //     //             } else {
-        //     //                 alert('da失败！')
-        //     //             }
-        //     //         })
-        //     //     } else {
-        //     //         alert('RERER！')
-        //     //     }
-        //     // } catch (error) {
-        //     //     console.log(error)
-        //     // }
-        // },
-
-        // // 登录旧
-        // async login(e: { preventDefault: () => void; }) {
-        //     e.preventDefault();
-        //     // 检验密码是否正确的逻辑
-        //     // 登录成功后修改isLoggedIn的值为true
-        //     if (this.username === '' || this.pwd === '') {
-        //         window.alert('账号和密码不能为空！')
-        //     } else {
-        //         const data = {
-        //             'username': this.username,
-        //             'password': this.pwd
-        //         }
-        //         console.log(data)
-        //         this.$router.push('/')
-        //         // routes.push({ path: '/teams' })
-        //         // const response = await fetch('http://dev-cn.your-api-server.com/user/login', {
-        //         //     method: 'GET',
-        //         //     headers: {
-        //         //         'Content-Type': 'application/json',
-        //         //     },
-        //         //     body: JSON.stringify(data),
-        //         // });
-        //         // if (response.status === 200) {
-        //         //     response.json().then(res => {
-        //         //         if (res.code === 200) {
-        //         //             localStorage.setItem('isLoggedIn', 'true');
-        //         //             //userStore.getUserInfo(username.value)
-        //         //             sessionStorage.setItem('username', this.username)
-        //         //             sessionStorage.setItem('userId', res.data.userId)
-        //         //             // router.push({ path: '/teams' })
-        //         //         } else {
-        //         //             window.alert('账号或密码错误！请重新输入')
-        //         //         }
-        //         //     })
-        //         // } else {
-        //         //     window.alert('EEE')
-        //         // }
-        //     }
-        // },
-
         // 邮箱验证码
         sendVerificationCode() {
             // 在这里执行发送邮箱验证码的逻辑
             // 设置 verificationCodeSent 为 true，防止重复发送验证码
             console.log('发送邮箱验证码', this.email);
-            sendCode(this.email).then((res: Type.SendCodeReturn) => {
-                if (res.flag == true) {
-                    this.verificationCodeSent = true;
+            if (this.email == '') {
+                ElMessage({
+                    message: `请输入邮箱！`,
+                    type: 'error',
+                })
+                return
+            } else {
+                const myreg = /^([a-zA-Z]|[0-9])(\w|\-)+@[a-zA-Z0-9]+\.([a-zA-Z]{2,4})$/;
+                if (!myreg.test(this.email)) {
                     ElMessage({
-                        message: `验证码发送成功！`,
-                        type: 'success',
+                        message: `请检查邮箱格式！`,
+                        type: 'warning',
                     })
+                    return
                 } else {
-                    ElMessage({
-                        message: `验证码发送失败！`,
-                        type: 'error',
+                    sendCode(this.email).then((res: Type.SendCodeReturn) => {
+                        if (res.flag == true) {
+                            this.verificationCodeSent = true;
+                            ElMessage({
+                                message: `验证码发送成功！`,
+                                type: 'success',
+                            })
+                        } else {
+                            ElMessage({
+                                message: `验证码发送失败！`,
+                                type: 'error',
+                            })
+                            console.log(res)
+                        }
+                    }).catch(err => {
+                        console.log(err)
                     })
-                    console.log(res)
                 }
-            }).catch(err => {
-                console.log(err)
-            })
+            }
         },
-
-        // 登录/忘记密码页面翻转
-        // handleForgotPassword() {
-        //     //跳转到忘记密码页面
-        //     // router.replace('/forgetpassword')
-        //     this.playFlip = !this.playFlip
-        //     this.playForget = !this.playForget
-        // },
-
-        // 重置密码
-        // resetPassword() {
-        //     if (this.newPassword !== this.confirmNewPassword) {
-        //         alert('密码和确认密码不匹配');
-        //         return;
-        //     }
-        //     // 在这里执行重置密码逻辑，可以发送请求给服务器或者执行其他操作
-        //     console.log('重置密码信息：', {
-        //         username: this.f_username,
-        //         email: this.f_email,
-        //         verificationCode: this.verificationCode,
-        //         newPassword: this.newPassword,
-        //     });
-        //     ElMessage({
-        //         message: `重置成功！`,
-        //         type: 'success',
-        //         offset: 100,
-        //     })
-        //     this.f_username = ''
-        //     this.f_email = ''
-        //     this.verificationCode = ''
-        //     this.verificationCodeSent = false
-        //     this.newPassword = ''
-        //     this.confirmNewPassword = ''
-        //     // this.handleForgotPassword()
-        // },
     }
 }
 </script>
@@ -380,7 +225,8 @@ export default {
 <style scoped>
 .background {
     position: fixed;
-    background-image: url("https://scholarin.cn/signin/static/hky_signin_page_bg.webp");
+    /* background-image: url("https://scholarin.cn/signin/static/hky_signin_page_bg.webp"); */
+    background-image: url("https://scholarin.cn/signin/static/pubscholar_signin_page_bg.webp");
     top: 0;
     left: 0;
     bottom: 0;
